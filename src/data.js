aee1b60252d86bd9246bfcf4739aa6e98b20fadf
Saikiran Maragouni <100951913+saikiran-maragouni@users.noreply.github.com>
2023-08-29 07:32:08 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://shorturl.at/klCDE",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd"
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed"
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4"
+  }
+];
+
+export const categories = [
+  {
+    id: 1,
+    img:
+      "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SHIRT STYLE!"
+  },
+  {
+    id: 2,
+    img:
+      "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "LOUNGEWEAR LOVE"
+  },
+  {
+    id: 3,
+    img:
+      "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "LIGHT JACKETS"
+  }
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img:
+      "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png"
+  },
+  {
+    id: 2,
+    img: "https://shorturl.at/afAM8"
+  },
+  {
+    id: 3,
+    img:
+      "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png"
+  },
+  {
+    id: 4,
+    img: "https://shorturl.at/hlqz0"
+  },
+  {
+    id: 5,
+    img: "https://shorturl.at/htHT9"
+  },
+  {
+    id: 6,
+    img: "https://shorturl.at/deqWZ"
+  },
+  {
+    id: 7,
+    img: "https://shorturl.at/crPX5"
+  },
+  {
+    id: 8,
+    img:
+      "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png"
+  }
+];
